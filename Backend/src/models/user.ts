e48dc2f705eb9603be8d57f7e144e248dc441c79
Mado Ngome <maladongom104@gmail.com>
2024-08-03 11:00:49 +0000

    connecte database and adding user model

---
+++ Backend/src/models/user.ts
@@ -0,0 +1,7 @@
+export interface user {
+  id?: number;
+  firstname: string;
+  lastname: string;
+  email: string;
+  password: string;
+}
---
+++ Backend/src/services/database.ts
@@ -0,0 +1,38 @@
+import sqlite3 from "sqlite3";
+// const sqlite3 = require("sqlite3").verbose();
+export class database {
+  db: sqlite3.Database;
+  constructor() {
+    // add a callback function to handle requeste to the database
+    this.db = new sqlite3.Database(process.env.SQLITE_DB || "school", (err) => {
+      if (err) {
+        console.log("an error occurs to the database", err);
+      } else {
+        console.log("database created successfully");
+        this.createTable();
+      }
+    });
+  }
+  // also add here a callback to check if the table is created or not
+  private createTable() {
+    this.db.run(
+      `
+      create table if not exists users (
+      id integer PRIMARY KEY AUTOINCREMENT,
+      firstname TEXT,
+      lastname TEXT,
+      email TEXT,
+      password TEXT,
+      createAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+      )
+      `,
+      (err) => {
+        if (err) {
+          console.log("table not created", err);
+        } else {
+          console.log("table created successfully");
+        }
+      }
+    );
+  }
+}
